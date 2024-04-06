b0ccb69bd0e64b07fef6cc7456f0c60467b5c5d9
PantilimonAdrian <pantilimon_adrian73@yahoo.com>
2024-04-06 16:17:07 +0300

    added dependencies

--- FrontEnd/my-app/src/App.js
+++ FrontEnd/my-app/src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from "./pages/home/Home";
+import Greenhouses from "./pages/greenhouses/greenhouses";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import HelloWorld from "./HelloWorld";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
                 <Route path="login" element={<Home/>} />
                 <Route path="singup" element={<Home/>} />
                 <Route path="store" element={<Home/>} />
+                <Route path="hello" element={<HelloWorld/>} />
+                <Route path="greenhouses" element={<Greenhouses/>} />
             </Route>
             <Route index element={<Home/>}/>
           </Routes>
--- GreenHarvest/views.py
+++ GreenHarvest/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from data.green_house_data import GREENHOUSE_DATA
+@api_view(['GET'])
+def hello_world(request):
+    return Response({'message': 'Hello, world!'})
 
-# Create your views here.
+@api_view(['GET'])
+def view_green_houses(request):
+    """Function used to view all greenhouse houses information"""
+
+    return Response(
+        {"greenhouse houses": GREENHOUSE_DATA}
+    )
