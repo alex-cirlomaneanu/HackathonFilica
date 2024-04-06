d7c1de6f540710136742ed127b3bb2db98f6c3bf
Alexandru Cîrlomăneanu <acirlomaneanu@stud.acs.upb.ro>
2024-04-06 13:00:01 +0300

    fe2

---
+++ FrontEnd/my-app/src/components/Layout/Header.js
@@ -0,0 +1,43 @@
+import { NavLink, Link } from 'react-router-dom';
+import {NavDropdown, Image, Nav, Navbar} from "react-bootstrap";
+import React from 'react';
+
+
+export const Header = () => {
+    return (
+        <>
+            <header className='header-bottom py-2'>
+                <div className='container'>
+                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                        <div className="container-fluid">
+                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                            <div className="collapse navbar-collapse" id="navbarNav">
+                                <ul className="navbar-nav ">
+                                    <li className="nav-item mx-5">
+                                        <Link to='/' className='d-flex align-items-center gap-15 text-black'>
+                                            <p className='mb-0'>Home</p>
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item mx-5">
+                                        <Link to='/login' className='d-flex align-items-center gap-10 text-black'>
+                                            <p className='mb-0'>LogIn</p>
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item mx-5">
+                                        <Link to='/store' className='d-flex align-items-center gap-10 text-black'>
+                                            <p className='mb-0'>Store</p>
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
---
+++ FrontEnd/my-app/src/pages/home/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Home.css';
+import Header from "../../components/Layout/Header";
+import Footer from "../../components/Layout/Footer"; // Fișierul pentru stilizare
+
+function Home() {
+  return (
+    <div className="home-container">
+        {/*<Header />*/}
+      <header>
+        <h1>Welcome to Our Page!</h1>
+        <p>Discover innovative solutions for farm sustainability using circular economy and ecology.</p>
+      </header>
+      <section>
+        <h2>How Can We Help?</h2>
+        <p>Explore our tools to optimize resources, manage waste, and promote sustainable agriculture.</p>
+      </section>
+      <section>
+        <h2>About Us</h2>
+        <p>We are a dedicated team developing solutions for farmers who want to contribute to protecting the environment. Read more about our mission and values.</p>
+      </section>
+      {/*<Footer/>*/}
+    </div>
+  );
+}
+
+export default Home;
